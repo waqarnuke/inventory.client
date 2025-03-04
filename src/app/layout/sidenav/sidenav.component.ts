d3aa9e0eb41f29d10231860085a176d90ed7dd91
waqarnuke <wnukedev@gmail.com>
2025-03-03 23:41:10 -0600

    update nev

--- src/app/layout/sidenav/sidenav.component.ts
+++ src/app/layout/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
+import { NgIf } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -9,16 +10,26 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [RouterOutlet,MatToolbarModule, MatButtonModule, MatIconModule,MatSidenavModule,MatListModule,RouterModule],
+  imports: [RouterOutlet,MatToolbarModule, MatButtonModule, MatIconModule,MatSidenavModule,MatListModule,RouterModule,NgIf],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
   showFiller = true;
+  isMobile: boolean = window.innerWidth < 768;
 
   menuItems : any[] = [
     {route: 'dashboard', icon: 'dashboard', label:'Dashboard'},
-    {route: 'inventory', icon: 'shopping_bag', label:'Inventory'}, 
+    {
+      route: 'inventory', 
+      icon: 'shopping_bag', 
+      label:'Inventory',
+      children: [
+            { route: 'inventory/products', label: 'Products' },
+            { route: 'inventory/stock', label: 'Stock' },
+            { route: 'inventory/suppliers', label: 'Suppliers' }
+      ]
+    }, 
     {route: 'buying', icon: 'shopping_cart ', label:'Buying'},
     {route: 'selling', icon: 'shopping_bag' , label:'Selling'},
     {route: 'Employee', icon: 'people' , label:'Employee'},
@@ -26,4 +37,17 @@ export class SidenavComponent {
     {route: 'reports', icon: 'bar_chart' , label:'Reports'},
     {route: 'Settings', icon: 'settings' , label:'Settings'}
   ];
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth < 768;
+  }
+  toggleMenu(item: any) {
+    item.expanded = !item.expanded;
+  }
+  
 }
